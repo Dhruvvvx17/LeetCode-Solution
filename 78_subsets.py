2309a93e65450cb887a22e6213372aee01a3e83f
Dhruv Vohra <dhruv.v405@gmail.com>
2022-01-12 20:10:48 +0530

    2 Questions 12th Jan, 2022

--- 78_subsets.py
+++ 78_subsets.py
@@ -1,10 +1,13 @@
 from itertools import combinations
+from typing import List
 
 
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        for i in range(len(nums)+1):
-            res += list(map(list,combinations(nums,i)))
-        
-        return(res)
+def subsets(nums: List[int]) -> List[List[int]]:
+    res = []
+    for i in range(len(nums)+1):
+        res += list(map(list,combinations(nums,i)))
+    
+    return(res)
+
+if __name__ == "__main__":
+    print(subsets([1,2,3]))
