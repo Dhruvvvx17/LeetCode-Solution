54dccfe4c033e38979df582f21cb0e28fde56d3f
Dhruv Vohra <dhruv.v405@gmail.com>
2022-02-15 12:34:03 +0530

    1 Question Feb 15, 2022

---
+++ Questions 121 - 130/128_longestConsecutiveSequence.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestConsecutive(vector<int>& nums) {
+    if(nums.size()==0) return 0;
+    
+    // sort the vector
+    sort(nums.begin(),nums.end());
+
+    // current and global result
+    int curr_longest = 1, global_longest = 0;
+    
+    for (int i=1; i<nums.size(); i++){
+        if(nums[i] - nums[i-1] == 1) curr_longest++;
+        
+        else if(nums[i] - nums[i-1] > 1){
+            global_longest = (curr_longest > global_longest) ? curr_longest : global_longest;
+            curr_longest = 1;
+        }
+    }
+
+    global_longest = (curr_longest > global_longest) ? curr_longest : global_longest;
+    return global_longest;        
+}
+
+int main(){
+    int n, temp;
+    vector<int> nums;
+
+    cout<<"Enter number of elements: ";
+    cin>>n;
+
+    while(n){
+        cout<<"Enter element: ";
+        cin>>temp;
+        nums.push_back(temp);
+        n--;
+    }
+
+    cout<<"Longest consecutive sequence = "<<longestConsecutive(nums);
+
+    return 0;
+}
