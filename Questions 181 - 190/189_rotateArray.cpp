f2b439802709104857053cc363d367e56a8d8fdc
Dhruv Vohra <dhruv.v405@gmail.com>
2022-06-04 00:21:44 +0530

    1 Question June 4, 2022

---
+++ Questions 181 - 190/189_rotateArray.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int>& nums, int k) {
+    int steps = k % nums.size();
+    std::rotate(nums.rbegin(), nums.rbegin() + k%nums.size(), nums.rend());
+    return;
+}
+
+void printVector(vector<int>& nums) {
+    for(int i=0; i<nums.size(); i++) {
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+    int n;
+    cout<<"Enter number of elements: ";
+    cin>>n;
+
+    vector<int> nums(n);
+    cout<<"Enter array elements: "<<endl;
+    for(int i=0; i<n; i++) {
+        cout<<"Element "<<i+1<<" : ";
+        cin>> nums[i];
+    }
+
+    int k;
+    cout<<"Enter the number of right rotations: ";
+    cin>>k;
+
+    cout<<"Original array: ";
+    printVector(nums);
+
+    rotate(nums, k);
+    
+    cout<<"Rotated array: ";
+    printVector(nums);
+
+    return 0;
+}
