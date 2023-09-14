736d216a5020eb626f118cdf263fd5d7e383901c
Dhruv Vohra <dhruv.v405@gmail.com>
2023-09-14 11:54:52 -0700

    howSum modification using python dynamic return type adv

--- Dynamic Programming/Python/howSum.py
+++ Dynamic Programming/Python/howSum.py
@@ -1,35 +1,36 @@
-#  given a target an an array of non-negative integers, return a combination of values that sum up to the target value.
-#  the numbers can be used repeatedly. If no combination possible return NULL
+# given a target an an array of non-negative integers, return a combination of values that sum up to the target value.
+# the numbers can be used repeatedly. If no combination possible return NULL
 
-#  Time Complexity brute force is O(n^m * m) - (branching factor ^ tree height * copying array elements)
-#  Time Complexity with memoization O(m*n * m) - m is the target and n is the number of elements. (Second m is copying array elements)
+# Time Complexity brute force is O(n^m * m) - (branching factor ^ tree height * copying array elements)
+# Time Complexity with memoization O(m*n * m) - m is the target and n is the number of elements. (Second m is copying array elements)
+
+# as python allowes different return type for the same function (None or a list) as opposed to C++, it can be used to 
+# simply return None or best list until now rather that true/false and a list in method params that's modified.
 
 cache = {}
-def howSum(target, arr, res):
+def howSum(target, arr):
 
     # edge cases
     if(target == 0):
-        return True
+        return []
     if(target < 0):
-        return False
+        return None
 
     # cache check
     if(target in cache):
-        savedSolution = cache[target]
-        if(len(savedSolution) == 0):
-            return False
-        return True
+        return cache[target]
 
     # if not found in cache
     for num in arr:
-        if(howSum(target-num, arr, res)):
+        res = howSum(target-num, arr)
+        if not(res == None):
             res.append(num)
             cache[target] = res[:]
-            return True
+            return cache[target]
 
     # this branch did not lead to a solution
-    cache[target] = []
-    return False
+    cache[target] = None
+    return cache[target]
 
 
 if __name__ == "__main__":
@@ -42,9 +43,9 @@ if __name__ == "__main__":
         i += 1
     
     target = int(input("Enter target: "))
-    res = []
 
-    if(howSum(target, arr, res)):
+    res = howSum(target, arr)
+    if(res):
         print(f"{target} can be generated from array elements {res}")
     else:
         print(f"{target} cannot be generated from array")
